4e4bda656d02169576ca41a5c96496c47121b6ec
Natalia Niko <74614594+TashaNIKO@users.noreply.github.com>
2022-09-30 22:06:06 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { data} from "./data";
+import './App.css';
+
+function App() {
+const [gifts, setGifts]=useState(data);
+
+const removeItem =(id)=> {
+  let newGifts=gifts.filter(gift=>gift.id !==id);
+  setGifts(newGifts)
+}
+
+  return (
+  <div>
+    <div className="container">
+      <h1>List of {gifts.length} gifts</h1>
+    </div>
+    {gifts.map((element => {
+    const {id, gift, image} = element;
+  return (<div key={id}>
+    <div className="container">
+      <h2>{id} - {gift}</h2>
+    </div>
+    <div className="container">
+      <img src={image} width="300px" alt="gift"/>
+      </div>
+    <div className="container">
+      <button onClick={()=> removeItem(id)}>remove</button>
+        </div>
+  </div>)
+    }))}
+    <div className="container">
+      <button onClick={()=>setGifts([])}>delete all</button>
+    </div>
+  </div>
+  );
+}
+
+export default App;
